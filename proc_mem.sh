9260842b4345cae96dfac428151ea21a190edaf5
AlexSem88 <al.semenenko88@yandex.ru>
2025-06-05 10:10:39 +0300

    update proc_mem.sh

--- proc_mem.sh
+++ proc_mem.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Количество процессов для отображения
-NUM_PROCESSES=20
+# Используем переданный аргумент или значение по умолчанию
+NUM_PROCESSES=${1:-20}
 
-# Команда мониторинга
 CMD="free -h; echo; ps -ax -o pid,comm,pcpu,pmem,rss,user --sort=-pmem | head -n $((NUM_PROCESSES + 1))"
 
-# Запуск мониторинга с обновлением каждые 2 секунды
 watch -n 2 "$CMD"
